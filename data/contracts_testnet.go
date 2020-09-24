ab4f62962a3930e4e87b714d9abf8a91892f0c9e
Bobo <dongkaige@gmail.com>
2020-09-24 15:35:03 +0800

    refa: rm Balancer hard codec contract addrs

--- data/contracts_testnet.go
+++ data/contracts_testnet.go
@@ -10,17 +10,18 @@ const (
 
 // Contract ABI:
 const (
-	UniswapV2        = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
-	Bancor           = "0x2F9EC37d6CcFFf1caB21733BdaDEdE11c823cCB0"
-	OneInch          = "0xC586BeF4a0992C495Cf22e1aeEE4E446CECDee0E"
-	SushiSwap        = "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F"
-	Kyber            = "0x818E6FECD516Ecc3849DAf6845e3EC868087B755"
-	MooniswapFactory = "0x71CD6666064C3A1354a3B4dca5fA1E2D3ee7D303"
-	Paraswap         = "0x86969d29F5fd327E1009bA66072BE22DB6017cC6" // TODO: 来自estimate_tx_fee package
-	Oasis            = "0x794e6e91555438aFc3ccF1c5076A74F42133d08D"
-	Dforce           = "0x03eF3f37856bD08eb47E2dE7ABc4Ddd2c19B60F2"
-	Paraswap2        = "0x12295f06DA62693086F5DA45b78e20B778060853" // 来自Handler package
-	UniswapV2Factory = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f"
+	UniswapV2               = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
+	Bancor                  = "0x2F9EC37d6CcFFf1caB21733BdaDEdE11c823cCB0"
+	OneInch                 = "0xC586BeF4a0992C495Cf22e1aeEE4E446CECDee0E"
+	SushiSwap               = "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F"
+	Kyber                   = "0x818E6FECD516Ecc3849DAf6845e3EC868087B755"
+	MooniswapFactory        = "0x71CD6666064C3A1354a3B4dca5fA1E2D3ee7D303"
+	Paraswap                = "0x86969d29F5fd327E1009bA66072BE22DB6017cC6"
+	Oasis                   = "0x794e6e91555438aFc3ccF1c5076A74F42133d08D"
+	Dforce                  = "0x03eF3f37856bD08eb47E2dE7ABc4Ddd2c19B60F2"
+	UniswapV2Factory        = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f"
+	BalancerRegistry        = "0x65e67cbc342712DF67494ACEfc06fe951EE93982"
+	BalancerExchangeProxyV2 = "0x3E66B66Fd1d0b02fDa6C811Da9E0547970DB2f21"
 )
 
 // UniswapV2
