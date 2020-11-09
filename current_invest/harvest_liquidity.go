28620f22fe824268eee28c7e74b4be2fca750a98
Bobo <dongkaige@gmail.com>
2020-11-09 19:14:51 +0800

    refa: change harvest pool names

--- current_invest/harvest_liquidity.go
+++ current_invest/harvest_liquidity.go
@@ -17,23 +17,23 @@ var (
 )
 
 var harvestVault = map[string]string{
-	"weth_vault":      "0xFE09e53A81Fe2808bc493ea64319109B5bAa573e", // 0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2; Wrapped Ether
-	"dai_vault":       "0xab7FA2B2985BCcfC13c6D86b1D5A17486ab1e04C", // 0x6B175474E89094C44Da98b954EedeAC495271d0F; Dai Stablecoin
-	"usdc_vault":      "0xf0358e8c3CD5Fa238a29301d0bEa3D63A17bEdBE", // 0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48; USD Coin
-	"usdt_vault":      "0x053c80eA73Dc6941F518a68E2FC52Ac45BDE7c9C", // 0xdac17f958d2ee523a2206206994597c13d831ec7; Tether: USDT Stablecoin
-	"tusd_vault":      "0x7674622c63Bee7F46E86a4A5A18976693D54441b", // 0x0000000000085d4780b73119b644ae5ecd22b376; TrueUSD
-	"wbtc_vault":      "0x5d9d25c7C457dD82fc8668FFC6B9746b674d4EcB", // 0x2260fac5e5542a773aa44fbcfedf7c193bc2c599; Wrapped BTC: WBTC Token
-	"renbtc_vault":    "0xC391d1b08c1403313B0c28D47202DFDA015633C4", // 0xeb4c2781e4eba804ce9a9803c67d0893436bb27d; Ren: renBTC Token
-	"crvrenbtc_vault": "0x9aA8F427A17d6B0d91B6262989EdC7D45d6aEdf8", // 0x49849c98ae39fff122806c06791fa73784fb3675; Curve.fi: renCrv Token
+	"WETH vault":       "0xFE09e53A81Fe2808bc493ea64319109B5bAa573e", // 0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2; Wrapped Ether
+	"DAI vault":        "0xab7FA2B2985BCcfC13c6D86b1D5A17486ab1e04C", // 0x6B175474E89094C44Da98b954EedeAC495271d0F; Dai Stablecoin
+	"USDC vault":       "0xf0358e8c3CD5Fa238a29301d0bEa3D63A17bEdBE", // 0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48; USD Coin
+	"USDT vault":       "0x053c80eA73Dc6941F518a68E2FC52Ac45BDE7c9C", // 0xdac17f958d2ee523a2206206994597c13d831ec7; Tether: USDT Stablecoin
+	"TUSD vault":       "0x7674622c63Bee7F46E86a4A5A18976693D54441b", // 0x0000000000085d4780b73119b644ae5ecd22b376; TrueUSD
+	"WBTC vault":       "0x5d9d25c7C457dD82fc8668FFC6B9746b674d4EcB", // 0x2260fac5e5542a773aa44fbcfedf7c193bc2c599; Wrapped BTC: WBTC Token
+	"RenBTC vault":     "0xC391d1b08c1403313B0c28D47202DFDA015633C4", // 0xeb4c2781e4eba804ce9a9803c67d0893436bb27d; Ren: renBTC Token
+	"CRV:RenBTC vault": "0x9aA8F427A17d6B0d91B6262989EdC7D45d6aEdf8", // 0x49849c98ae39fff122806c06791fa73784fb3675; Curve.fi: renCrv Token
+	"yCRV vault":       "0x0FE4283e0216F94f5f9750a7a11AC54D3c9C38F3",
 }
 
 var harvestVault2 = map[string]string{
-	"weth_dai_lp_vault":  "0x307E2752e8b8a9C29005001Be66B1c012CA9CDB7", // 0xa478c2975ab1ea89e8196811f51a7b7ade33eb11; Uniswap V2: DAI 2
-	"weth_usdc_lp_vault": "0xA79a083FDD87F73c2f983c5551EC974685D6bb36", // 0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc; Uniswap V2: USDC 3
-	"weth_usdt_lp_vault": "0x7DDc3ffF0612E75Ea5ddC0d6Bd4e268f70362Cff", // 0x0d4a11d5EEaaC28EC3F61d100daF4d40471f1852; Uniswap V2: USDT 2
-	"weth_wbtc_lp_vault": "0x01112a60f427205dcA6E229425306923c3Cc2073", // 0xbb2b8038a1640196fbe3e38816f3e67cba72d940; Uniswap V2: WBTC 2
-
-	"wbtc_tbtc_lp_vault": "0xF553E1f826f42716cDFe02bde5ee76b2a52fc7EB", // 0x2dbc7dd86c6cd87b525bd54ea73ebeebbc307f68; SushiSwap LP Token (SLP)
+	"UNI:ETH-DAI vault":     "0x307E2752e8b8a9C29005001Be66B1c012CA9CDB7", // 0xa478c2975ab1ea89e8196811f51a7b7ade33eb11; Uniswap V2: DAI 2
+	"UNI:ETH-USDC vault":    "0xA79a083FDD87F73c2f983c5551EC974685D6bb36", // 0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc; Uniswap V2: USDC 3
+	"UNI:ETH-USDT vault":    "0x7DDc3ffF0612E75Ea5ddC0d6Bd4e268f70362Cff", // 0x0d4a11d5EEaaC28EC3F61d100daF4d40471f1852; Uniswap V2: USDT 2
+	"UNI:ETH-WBTC vault":    "0x01112a60f427205dcA6E229425306923c3Cc2073", // 0xbb2b8038a1640196fbe3e38816f3e67cba72d940; Uniswap V2: WBTC 2
+	"SUSHI:WBTC-TBTC vault": "0xF553E1f826f42716cDFe02bde5ee76b2a52fc7EB", // 0x2dbc7dd86c6cd87b525bd54ea73ebeebbc307f68; SushiSwap LP Token (SLP)
 }
 
 func GetHarvestLiquidity(userAddr common.Address) ([]*UserLiquidityInvest, error) {
--- current_invest/harvest_staking.go
+++ current_invest/harvest_staking.go
@@ -22,27 +22,27 @@ var harvestStakingPool1 = map[string]string{
 
 // NoMintRewardPool -> lpToken -> UnderlyingToken -> TokenValue
 var harvestStakingPool2 = map[string]string{
-	strings.ToLower("0x75071F2653fBC902EBaff908d4c68712a5d1C960"): "Uni ETH-USDT", // Uni ETH-USDT; fUNISWAP_LP Farm
-	strings.ToLower("0x156733b89Ac5C704F3217FEe2949A9D4A73764b5"): "Uni ETH-USDC", // Uni ETH-USDC; fUNISWAP_LP
-	strings.ToLower("0x7aeb36e22e60397098C2a5C51f0A5fB06e7b859c"): "Uni ETH-DAI",  // Uni ETH-DAI; fUNISWAP_LP
-	strings.ToLower("0xF1181A71CC331958AE2cA2aAD0784Acfc436CB93"): "Uni ETH-WBTC", // Uni ETH-WBTC; fUNISWAP_LP
+	strings.ToLower("0x75071F2653fBC902EBaff908d4c68712a5d1C960"): "Uni:ETH-USDT", // Uni ETH-USDT; fUNISWAP_LP Farm
+	strings.ToLower("0x156733b89Ac5C704F3217FEe2949A9D4A73764b5"): "Uni:ETH-USDC", // Uni ETH-USDC; fUNISWAP_LP
+	strings.ToLower("0x7aeb36e22e60397098C2a5C51f0A5fB06e7b859c"): "Uni:ETH-DAI",  // Uni ETH-DAI; fUNISWAP_LP
+	strings.ToLower("0xF1181A71CC331958AE2cA2aAD0784Acfc436CB93"): "Uni:ETH-WBTC", // Uni ETH-WBTC; fUNISWAP_LP
 }
 
 // NoMintRewardPool -> lpToken -> fToken -> undelyingToken's Price
 var harvestStakingPool3 = map[string]string{
-	strings.ToLower("0xeC56a21CF0D7FeB93C25587C12bFfe094aa0eCdA"): "TUSD Farm",      // TUSD Farm; fTUSD
-	strings.ToLower("0x3DA9D911301f8144bdF5c3c67886e5373DCdff8e"): "WETH Farm",      // WETH Farm; fWETH
-	strings.ToLower("0x4F7c28cCb0F1Dbd1388209C67eEc234273C878Bd"): "USDC Farm",      // USDC Farm
-	strings.ToLower("0x15d3A64B2d5ab9E152F16593Cdebc4bB165B5B4A"): "DAI Farm",       // DAI Farm
-	strings.ToLower("0x6ac4a7AB91E6fD098E13B7d347c6d4d1494994a2"): "USDT Farm",      // USDT Farm; fUSDT
-	strings.ToLower("0x917d6480Ec60cBddd6CbD0C8EA317Bcc709EA77B"): "WBTC Farm",      // WBTC Farm; fWBTC
-	strings.ToLower("0x7b8Ff8884590f44e10Ea8105730fe637Ce0cb4F6"): "RENBTC Farm",    // RENBTC Farm; fRENBTC
-	strings.ToLower("0xA3Cf8D1CEe996253FAD1F8e3d68BDCba7B3A3Db5"): "CRVRENBTC Farm", // CRVRENBTC Farm; fCRVRENWBTC
-	strings.ToLower("0x6D1b6Ea108AA03c6993d8010690264BA96D349A8"): "yCRV Farm",      // yCRV Farm; fYCRV
+	strings.ToLower("0xeC56a21CF0D7FeB93C25587C12bFfe094aa0eCdA"): "TUSD Farm",       // TUSD Farm; fTUSD
+	strings.ToLower("0x3DA9D911301f8144bdF5c3c67886e5373DCdff8e"): "WETH Farm",       // WETH Farm; fWETH
+	strings.ToLower("0x4F7c28cCb0F1Dbd1388209C67eEc234273C878Bd"): "USDC Farm",       // USDC Farm
+	strings.ToLower("0x15d3A64B2d5ab9E152F16593Cdebc4bB165B5B4A"): "DAI Farm",        // DAI Farm
+	strings.ToLower("0x6ac4a7AB91E6fD098E13B7d347c6d4d1494994a2"): "USDT Farm",       // USDT Farm; fUSDT
+	strings.ToLower("0x917d6480Ec60cBddd6CbD0C8EA317Bcc709EA77B"): "WBTC Farm",       // WBTC Farm; fWBTC
+	strings.ToLower("0x7b8Ff8884590f44e10Ea8105730fe637Ce0cb4F6"): "RenBTC Farm",     // RENBTC Farm; fRENBTC
+	strings.ToLower("0xA3Cf8D1CEe996253FAD1F8e3d68BDCba7B3A3Db5"): "CRV:RenBTC Farm", // CRVRENBTC Farm; fCRVRENWBTC
+	strings.ToLower("0x6D1b6Ea108AA03c6993d8010690264BA96D349A8"): "yCRV Farm",       // yCRV Farm; fYCRV
 }
 
 var harvestStakingPool4 = map[string]string{
-	strings.ToLower("0x99b0d6641A63Ce173E6EB063b3d3AED9A35Cf9bf"): "Uniswap Farm", // Uniswap Farm; Deposit UNISWAP_LP Earn Farm
+	strings.ToLower("0x99b0d6641A63Ce173E6EB063b3d3AED9A35Cf9bf"): "LP:FARM-USDC", // Uniswap Farm; Deposit UNISWAP_LP Earn Farm
 }
 
 func GetHarvestStaked(userAddr common.Address) ([]*CurrentStakingInfo, error) {
